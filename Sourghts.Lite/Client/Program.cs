51e41173a9a99315b0cb9e68b43e14fd473dfaf5
Milkey Tan <819774989@qq.com>
2023-04-25 20:55:24 +0800

    memos

--- Sourghts.Lite/Client/Program.cs
+++ Sourghts.Lite/Client/Program.cs
@@ -14,7 +14,9 @@ namespace Sourghts.Lite
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddBlazoredLocalStorage(config =>
+                config.JsonSerializerOptions.WriteIndented = true
+            );
             builder.Services.AddBulmaRazor();
 
             await builder.Build().RunAsync();
